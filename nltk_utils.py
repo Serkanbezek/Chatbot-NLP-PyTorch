118967a92b04b2a94211cfb9c743fd49055a1706
Serkanbezek <96655729+Serkanbezek@users.noreply.github.com>
2022-03-04 00:34:20 +0300

    Created the model and trained the data

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,6 @@
 import nltk
-nltk.download('punkt')       #downloading a package with a pretrained tokenizer
+import numpy as np
+#nltk.download('punkt')       #downloading a package with a pretrained tokenizer
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -10,4 +11,15 @@ def stem(word):              #Generating the root form of the words
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype = np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
+
+
+
+
